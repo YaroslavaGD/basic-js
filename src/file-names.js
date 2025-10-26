dedfa23043dcc3fc956f22e78d5c384a3c74458c
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-10-26 23:46:20 +0100

    feat: add file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../lib');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function renameFiles(names) {
+  const listNames = new Map();
+
+  return names.map((name) => {
+    let newName = name;
+
+    if (listNames.has(name)) {
+      const newNumber = listNames.get(name) + 1;
+      listNames.set(name, newNumber);
+
+      newName = name + '(' + newNumber + ')';
+    }
+
+    listNames.set(newName, 0);
+
+    return newName; 
+  });
 }
 
 module.exports = {
