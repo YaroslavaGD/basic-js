c8aaa21984ed479d8eb2b67996e30061331e6ea2
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-11-09 18:17:36 +0100

    feat: add minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,9 +23,42 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const matrixRows = matrix.length;
+  const matrixCols = matrix[0].length;
+
+  const result = Array(matrixRows)
+    .fill(0)
+    .map(() => Array(matrixCols).fill(0));
+
+  const directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1],
+  ];
+
+  for (let i = 0; i < matrixRows; i += 1) {
+    for (let j = 0; j < matrixCols; j += 1) {
+      const isCurrentMine = matrix[i][j];
+      if (!isCurrentMine) continue;
+
+      result[i][j] = 1;
+
+      directions.forEach(([dx, dy]) => {
+        const x = i + dx;
+        const y = j + dy;
+        const isValidX = x >= 0 && x < matrixRows;
+        const isValidY = y >= 0 && y < matrixCols;
+        const isNeighborMine = matrix[x] ? matrix[x][y] : false;
+
+        if (isValidX && isValidY && !isNeighborMine) {
+          result[x][y] += 1;
+        }
+      });
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
