39570e8c4e5be1c8f1229d83882e50fb5b30851a
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-11-11 00:52:17 +0100

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,83 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  isLetter(char) {
+    return this.alphabet.includes(char);
+  }
+
+  indexOf(char) {
+    return this.alphabet.indexOf(char);
+  }
+
+  encryptedOrDecryptedChar(msgChar, keyChar, operation) {
+    const msgCharIndex = this.indexOf(msgChar);
+    const keyCharIndex = this.indexOf(keyChar);
+    let charIndex;
+
+    if (operation === 'encrypt') {
+      charIndex = (msgCharIndex + keyCharIndex) % this.alphabet.length;
+    } else if (operation === 'decrypt') {
+      charIndex = (msgCharIndex - keyCharIndex + this.alphabet.length) % this.alphabet.length;
+    } else {
+      throw new Error('Invalid operation!');
+    }
+
+    return this.alphabet[charIndex];
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    const msg = message.toUpperCase();
+    const k = key.toUpperCase();
+
+    let resultMsg = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < msg.length; i += 1) {
+      const msgChar = msg[i];
+      if (this.isLetter(msgChar)) {
+        const keyChar = k[keyIndex % k.length];
+        const encryptedChar = this.encryptedOrDecryptedChar(msgChar, keyChar, 'encrypt');
+
+        resultMsg += encryptedChar;
+        keyIndex += 1;
+      } else {
+        resultMsg += msgChar;
+      }
+    }
+
+    return this.isDirect ? resultMsg : resultMsg.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const msg = message.toUpperCase();
+    const k = key.toUpperCase();
+
+    let resultMsg = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < msg.length; i += 1) {
+      const msgChar = msg[i];
+      if (this.isLetter(msgChar)) {
+        const keyChar = k[keyIndex % k.length];
+        const decryptedChar = this.encryptedOrDecryptedChar(msgChar, keyChar, 'decrypt');
+
+        resultMsg += decryptedChar;
+        keyIndex += 1;
+      } else {
+        resultMsg += msgChar;
+      }
+    }
+
+    return this.isDirect ? resultMsg : resultMsg.split('').reverse().join('');
   }
 }
 
